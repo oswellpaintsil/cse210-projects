04035b329947a9158ab0e90c9ae39ce47d1ae5c0
oswellpaintsil <oswell.paintsil@gmail.com>
2025-03-11 20:40:50 +0000

    I updated the variable names to adhere to C# naming conventions.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
-        Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
-        Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
         Console.WriteLine();
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
