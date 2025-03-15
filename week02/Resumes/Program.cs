b32e0a0e4605dfe2b5e4b227ecec16bc03daae75
oswellpaintsil <oswell.paintsil@gmail.com>
2025-03-15 20:45:17 +0000

    Week 02 Resume Assignment Completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Console.WriteLine();
+        
+        Job job1 = new Job();
+        job1._company = "Fox Cooling";
+        job1._jobTitle = "Engineer";
+        job1._startYear = 2017;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Ceinsa";
+        job2._jobTitle = "Engineer";
+        job2._startYear = 2004;
+        job2._endYear = 2016;
+
+        myResume resume = new myResume();
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume._name = "Oswell Paintsil";
+
+        resume.DisplayResume();
+
     }
 }
---
+++ week02/Resumes/myResume.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+class myResume
+{
+    public string _name;
+    public List<Job>_jobs = new List<Job>();
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job j in _jobs)
+        {
+            j.DisplayJobDetails();
+        }
+    }
+
+}
