4ee4c968c331d67b76040a77fa6280b44cb1b38c
oswellpaintsil <oswell.paintsil@gmail.com>
2025-04-05 21:27:50 +0000

    W05 Team Activity: Mindfulness Design Stubs Completed.

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts;
+
+    public ListingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+        return new List<string>();
+    }
+}
