b5f5eb6329da723ecc4830c7cd763d6abaed4a66
oswellpaintsil <oswell.paintsil@gmail.com>
2025-04-12 14:08:00 +0000

    W06 Learning Activity: Polymorphism Completed.

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius)
+        : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        double pi = Math.PI;
+        double area = pi * _radius * _radius;
+
+        return area;
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.WriteLine();
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("black", 20));
+        shapes.Add(new Rectangle("white", 20, 10));
+        shapes.Add(new Circle("blue", 20));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
+        
     }
 }
