db86c31803bc2d5cb357f11f2630cc9dda9f5024
oswellpaintsil <oswell.paintsil@gmail.com>
2025-04-15 00:50:12 +0000

    W07 Assignment: Exercise Tracking Program Completed.

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+        
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine();
+
+        // Create activities
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 3.0),
+            new Cycling(new DateTime(2022, 11, 3), 30, 9.7),
+            new Swimming(new DateTime(2022, 11, 3), 30, 60)
+        };
+
+        // Display summaries
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
